e296fdc35963891efe2f042b5a61919d974cc943
airtonfranco <x572493@gruposantander.com>
2021-04-20 10:21:36 -0300

    #Aula-2 => configuração do typeORM

--- commands.ts
+++ commands.ts
@@ -7,3 +7,18 @@
  * yarn add ts-node-dev -D => translate typescript for node
  * yarn dev => run aplication
  */
+
+/**
+ * Baixar a extensão no crhome JSON Viewer => Opcional
+ */
+
+/**
+ * TypeORM => O typeORM é um framework de DB 
+ * que padroniza todos os tipos de DB na minha aplicação,
+ * ele é mais ultilizado com o TypeScript, 
+ * existe tambem o SequelizeORM que é mais ultilizado no node puro
+ * 
+ * Instalando o TypeORM
+ * yarn add typeorm reflect-metadata sqlite3 => instalando o sqlite3 via typeorm
+ * 
+ */
